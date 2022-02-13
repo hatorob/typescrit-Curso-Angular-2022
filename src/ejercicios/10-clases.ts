ecac9252e0b2b062f598217aa1565501277b0b3b
Hector Toro <alejotoro94@hotmail.com>
2022-02-13 15:51:09 -0500

    Constructor de una clase

--- src/ejercicios/10-clases.ts
+++ src/ejercicios/10-clases.ts
@@ -1,14 +1,26 @@
 // Clases
 
 
+//constructor de una clase
+
 class Hero {
-    /* private */ alterEgo: string;
-    /* public */ edad: number;
-    /* static */ nombreReal: number;  //Puedo acceder sin necesidad de crear una instancia
+    /* private */ //alterEgo: string;
+    /* public */ //edad: number;
+    /* static */ //nombreReal: number;  //Puedo acceder sin necesidad de crear una instancia
+    
+    constructor( 
+        public alterEgo: string,
+        public edad: number,
+        public nombreReal: string ) {
+        // Se llama cuando se crea una instancia de la clase
+        //Aquí podriamos inicializar las propiedades de mis clases
+        console.log("Heyy");
+        this.alterEgo = alterEgo;
+    }
 }
 
 /* Hero.nombreReal */  //por lo que es estatic
 
- const ironman = new Hero();
+ const ironman = new Hero('Ironman',20,'Alejandro');
 
  console.log(ironman);
